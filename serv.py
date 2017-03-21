1d8c35c9cac38b1f1c9717aeee6596641e943cfa
Ryan Cruz <Ryan Cruz>
2017-03-21 12:05:25 -0700

    first push

---
+++ serv.py
@@ -0,0 +1,10 @@
+ import soundcloud
+
+ self.client = soundcloud.Client(client_id="8c1cf28d0d2834808a2eda6645da717b", client_secret='67422b6c159a389c9cfed1a9607227ef')
+
+
+
+ ##waht did you do yesterday?
+ #what did you do toda?
+ #what is the plan for today?
+ #roadblocks?
