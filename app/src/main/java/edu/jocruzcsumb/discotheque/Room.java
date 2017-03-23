bf30576d0b12fe3617587c859f37369a23375350
Tommy <tha@csumb.edu>
2017-03-23 16:36:38 -0700

    created user chat message subclass of user

---
+++ app/src/main/java/edu/jocruzcsumb/discotheque/Room.java
@@ -0,0 +1,13 @@
+package edu.jocruzcsumb.discotheque;
+
+/**
+ * Created by Tommy on 3/23/2017.
+ */
+
+public class Room {
+
+    private String roomName;
+    private int numOfUsers;
+    //private
+
+}
