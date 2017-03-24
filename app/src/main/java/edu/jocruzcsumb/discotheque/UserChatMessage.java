e249353aba252d8eccde83ec0818c73a349352ef
Tommy <tha@csumb.edu>
2017-03-23 18:09:49 -0700

    added no-arg constructor to UserChatMessage class

--- app/src/main/java/edu/jocruzcsumb/discotheque/UserChatMessage.java
+++ app/src/main/java/edu/jocruzcsumb/discotheque/UserChatMessage.java
@@ -10,6 +10,11 @@ public class UserChatMessage extends User{
 
     private String chatMessage;
 
+    public UserChatMessage(){
+        super();
+        chatMessage = "";
+    }
+
     public UserChatMessage(String userName, String name, String email, String photo, String chatMessage){
         super(userName, name, email, photo);
         this.chatMessage = chatMessage;
