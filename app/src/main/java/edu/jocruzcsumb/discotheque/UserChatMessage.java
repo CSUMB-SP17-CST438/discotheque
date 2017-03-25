6a9934da5249bd211a292bc4d328a1fd1a62f699
Tommy <tha@csumb.edu>
2017-03-24 17:53:04 -0700

    added pub_time instance var

--- app/src/main/java/edu/jocruzcsumb/discotheque/UserChatMessage.java
+++ app/src/main/java/edu/jocruzcsumb/discotheque/UserChatMessage.java
@@ -9,20 +9,29 @@ import java.util.HashMap;
 public class UserChatMessage extends User{
 
     private String chatMessage;
+    private String pub_time;
 
     public UserChatMessage(){
         super();
         chatMessage = "";
+        pub_time = "";
     }
 
-    public UserChatMessage(String userName, String name, String email, String photo, String chatMessage){
-        super(userName, name, email, photo);
+    public UserChatMessage(String userName, String firstName, String lastName, String email, String photo, String description, String chatMessage, String pub_time){
+        super(userName, firstName, lastName, email, photo, description);
         this.chatMessage = chatMessage;
+        this.pub_time = pub_time;
     }
 
     public String getChatMessage(){
         return chatMessage;
     }
+
+    public String getPub_time(){
+        return pub_time;
+    }
+
+    public void setPub_time(String pub_time){this.pub_time = pub_time;}
 
     public void setChatMessage(String chatMessage){
         this.chatMessage = chatMessage;
