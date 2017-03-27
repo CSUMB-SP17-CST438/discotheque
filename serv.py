217dfd7c2335d489323f5cc12f6d753e8847af37
Ryan Cruz <Ryan Cruz>
2017-03-26 19:47:08 -0700

    added event functions for 'register', schema relationship fixes, and added functions

--- serv.py
+++ serv.py
@@ -4,9 +4,11 @@ from flask_socketio import join_room, leave_room
 import discoSounds as ds 
 import os
 
+
 public_room = 912837
 
 serv = flask.Flask(__name__)
+import schema as db
 socket = flask_socketio.SocketIO(serv)
 ##default route
 @serv.route('/')
@@ -17,22 +19,41 @@ def start():
 def on_connect():
 	print('client-connected')
 
-
+#adds a client to the public room
 @socket.on('join room')
 def on_join_room(data):
 	sessionid = request.sid
 	join_room(public_room)
 
+#should be called when connected to get a list of songs for the room
+#will take a 'genre' as an arguement, so this needs to be passed in
 @socket.on('get songs')
 def on_get_songs(data):
 	genre = data['genre']
 	songs = ds.getSongList(genre)
-	socket.emit('song list', songs,room='public')
+	ds.print(songs)
+	socket.emit('song list', songs,room=public_room)
 
 @socket.on('now playing')
 def on_now_playing(data):
 	current_song = data['current_song']
 	socket.emit('song to play', current_song, room=public_room)
+
+
+@socket.on('register')
+def on_register(data):
+	new_username = data['username']
+	new_password = data['password']
+	new_email = data['email']
+	new_fn = data['fname']
+	new_ln = data['lname']
+	if db.memberExists_by_username(new_username) and db.memberExists_by_email(new_email) is None:
+		db.registerMember(new_username,new_password,new_fn,new_ln,new_email)
+		print((new_username + "just registered!"))
+		socket.emit("registered successfully", {'message': "successfully registered!"})
+	else:
+		print("registration failure, user must already exist")
+
 
 if __name__ == '__main__':
 	socket.run(
