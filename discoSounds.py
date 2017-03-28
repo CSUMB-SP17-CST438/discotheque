b40a760114206931646793aff1b35f4209a35ac2
Ryan Cruz <Ryan Cruz>
2017-03-27 21:09:13 -0700

    ridiculoud fix to stream_url

--- discoSounds.py
+++ discoSounds.py
@@ -11,7 +11,7 @@ def getSongList(g):
 		#parse track information to what is relevant to us
 		username = t.user['username']
 		t_stream = client.get(scTracks[i].stream_url, allow_redirects=False)
-		new_track = {'title':t.title, 'creator_user':username,'track_permalink':t.permalink_url,'stream_url':str(t_stream),'artwork':t.artwork_url}
+		new_track = {'title':t.title, 'creator_user':username,'track_permalink':t.permalink_url,'stream_url':str(t_stream.location),'artwork':t.artwork_url}
 		trackList.append(new_track)
 		i+1
 	return trackList
@@ -20,10 +20,10 @@ def getSongList(g):
 def getSongURLLocation():
 	tracks = client.get('/tracks',genres='punk',limit=1,streamable=True)
 	track_stream = client.get(tracks[0].stream_url, allow_redirects=False)
-	print(tracks[0].stream_url)
+	# print(tracks[0].stream_url)
 
-	print("stream.loc:")
-	print(track_stream.location)
+	# print("stream.loc:")
+	# print(track_stream.location)
 
 
 getSongURLLocation()
