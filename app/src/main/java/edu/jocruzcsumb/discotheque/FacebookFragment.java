52c550a723b94de5ee8f99034973f3465c217664
Carsen Yates <fatburgeriii@gmail.com>
2017-04-05 15:42:50 -0700

    temp commit for switching over to tablet :)

--- app/src/main/java/edu/jocruzcsumb/discotheque/FacebookFragment.java
+++ app/src/main/java/edu/jocruzcsumb/discotheque/FacebookFragment.java
@@ -2,19 +2,24 @@ package edu.jocruzcsumb.discotheque;
 
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.facebook.share.Sharer;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
@@ -24,29 +29,59 @@ public class FacebookFragment extends Fragment {
     private LoginButton loginButton;
     private CallbackManager callbackManager;
 
-    Context context = getApplicationContext();
-    int duration = Toast.LENGTH_SHORT;
+    private final Context context = getApplicationContext();
 
-    private FacebookCallback<Sharer.Result> shareCallback = new FacebookCallback<Sharer.Result>() {
+    private static final int TOAST_DURATION = Toast.LENGTH_SHORT;
+    private static final String TAG = "Facebook API";
+
+    private FacebookCallback<LoginResult> loginCallback = new FacebookCallback<LoginResult>() {
+        //private FacebookCallback<Sharer.Result> shareCallback = new FacebookCallback<Sharer.Result>() {
         @Override
         public void onCancel() {
-            Toast toast = Toast.makeText(context, "Login Canceled", duration);
-            toast.show();
+            Toast.makeText(context, "Login Canceled", TOAST_DURATION).show();
+            Log.d(TAG, "Login Canceled");
         }
 
         @Override
         public void onError(FacebookException error) {
-            Toast toast2 = Toast.makeText(context,"Login Error",duration);
-            toast2.show();
+            Toast.makeText(context,"Login Error", TOAST_DURATION).show();
+            Log.d(TAG, "Login Error");
         }
 
         @Override
-        public void onSuccess(Sharer.Result result) {
-            Toast toast3 = Toast.makeText(context,"Login Success",duration);
-            toast3.show();
+        public void onSuccess(LoginResult result) {
+            Toast.makeText(context,"Login Success", TOAST_DURATION).show();
+            Log.d(TAG, "Login Success");
+            AccessToken token = result.getAccessToken();
+            JSONObject obj = new JSONObject();
+            try {
+                obj.put("fb_t", token.getToken());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
 
     };
+//    private FacebookCallback<Sharer.Result> shareCallback = new FacebookCallback<Sharer.Result>() {
+//        @Override
+//        public void onCancel() {
+//            Toast.makeText(context, "Share Canceled", TOAST_DURATION).show();
+//            Log.d(TAG, "Share Canceled");
+//        }
+//
+//        @Override
+//        public void onError(FacebookException error) {
+//            Toast.makeText(context,"Share Error",TOAST_DURATION).show();
+//            Log.d(TAG, "Share Error");
+//        }
+//
+//        @Override
+//        public void onSuccess(Sharer.Result result) {
+//            Toast.makeText(context,"Share Success",TOAST_DURATION).show();
+//            Log.d(TAG, "Share Success");
+//        }
+//
+//    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -61,26 +96,7 @@ public class FacebookFragment extends Fragment {
         loginButton.setFragment(this);
         callbackManager = CallbackManager.Factory.create();
         // Callback registration
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                Toast toast = Toast.makeText(context, "Logged In", duration);
-                toast.show();
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-                Toast toast2 = Toast.makeText(context,"Login Error",duration);
-                toast2.show();
-            }
-
-            @Override
-            public void onCancel() {
-                Toast toast = Toast.makeText(context, "Login Canceled", duration);
-                toast.show();
-            }
-
-            });
+        loginButton.registerCallback(callbackManager, loginCallback);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
