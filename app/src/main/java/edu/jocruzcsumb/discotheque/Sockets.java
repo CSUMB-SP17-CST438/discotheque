30d64d4d9ce2efa03e7f20ae2d02d5f77e094e2b
Ryan Cruz <jcrzry@users.noreply.github.com>
2017-04-06 15:02:47 -0700

    Revert "message"

--- app/src/main/java/edu/jocruzcsumb/discotheque/Sockets.java
+++ app/src/main/java/edu/jocruzcsumb/discotheque/Sockets.java
@@ -18,12 +18,11 @@ public class Sockets
 	private static Socket socket = null;
 
 	// TODO: Set to 0 for live server
-	private static final int SELECTED_SERVER = 2;
+	private static final int SELECTED_SERVER = 1;
 	// Append to this list if you want to run a different server :D
 	private static final String[] SERVERS = {
 			"https://disco-theque.herokuapp.com",
 			"http://carsen.ml",
-			"http://devev-jcrzry.c9users.io:8080"
 			//"http://INSERT IP",
 	};
 
