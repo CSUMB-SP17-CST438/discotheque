9c15f3c5a3b5acb205ee1f5a475e7c99d515dbd8
tommy <tha@csumb.edu>
2017-04-06 00:13:51 -0700

    created chat functionality

--- app/src/main/java/edu/jocruzcsumb/discotheque/Sockets.java
+++ app/src/main/java/edu/jocruzcsumb/discotheque/Sockets.java
@@ -25,7 +25,7 @@ public class Sockets
 
     public static String getServer()
 	{
-		return DEV_SERVER?"http://10.11.154.239":"https://disco-theque.herokuapp.com";
+		return DEV_SERVER?"https://tha01-tvanha01.c9users.io":"https://disco-theque.herokuapp.com";
 	}
 
 	//Checks to see if the current user is authed
