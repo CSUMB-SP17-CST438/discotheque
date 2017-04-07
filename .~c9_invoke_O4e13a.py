ed43e2688a4b7f670ebfa1fc696ec9bcc0630be1
Ryan Cruz <jcrzry@yahoo.com>
2017-04-07 00:31:10 +0000

    fix to test

---
+++ .~c9_invoke_O4e13a.py
@@ -0,0 +1,144 @@
+import flask
+import flask_socketio
+from flask_socketio import join_room, leave_room
+from flask import request
+import discoSounds as ds
+import os
+import json
+import datetime
+import requests
+import facebook
+
+public_room = 912837
+
+app = flask.Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL','postgresql://jcrzr:anchor99@localhost/postgres')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= 1
+
+from schema import *
+db.init_app(app)
+
+socket = flask_socketio.SocketIO(app)
+# default route
+
+
+@app.route('/')
+def start():
+    return flask.render_template('index.html')
+
+
+@socket.on('connect')
+def on_connect():
+    print('**************************************************client-connected**************************************************************')
+
+# adds a client to the public room
+
+
+@socket.on('join room')
+def on_join_room(data):
+    sessionid = request.sid
+    print("********************************************************joined room*************************************************************")
+    join_room(public_room)
+
+# should be called when connected to get a list of songs for the room
+# will take a 'genre' as an arguement, so this needs to be passed in
+
+
+@socket.on('get songs')
+def on_get_songs(data):
+    genre = data['genre']
+    songs = ds.getSongList(genre)
+    print("********************************************SONGLIST triggered******************************************************************")
+    socket.emit('song list', songs, room=public_room)
+
+
+@socket.on('song picked')
+def on_song_picked(data):
+    current_song = data['song']
+    stream_url_loc = ds.getSongURLLocation(current_song['id'])
+    current_song['stream_url'] = stream_url_loc
+    socket.emit('song to play', current_song, room=public_room)
+
+
+"""this listener is expecting key:pair list (i.e json) with either fb_t for facebook token, or google_t for google token
+	if user exists, function will validate and pass back user information and auth status. else, it will add user reference to schema"""
+
+@socket.on('login')
+def on_login(data):
+    print("**************************************triggered login*****************************************************************")
+    if 'fb_t' not in data:
+        response = requests.get(
+            'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' + data['google_t'])
+        json = response.json()
+        print(json)
+        fname = json['given_name'] 
+        lname = json['family_name']
+        link = json['picture']
+        email = json['email']
+        mem_found = memberExists_by_email(email)
+        if mem_found:
+            mem = getMember(email)
+            socket.emit("login status", {
+                        'authorized': 1, 'user': mem}, room=request.sid)
+        else:
+            new_mem = registerMember("",fname,lname,email,link).to_list()
+            socket.emit("login status", {'authorized': 1,'user':new_mem}, room=request.sid)
+
+#
+    if 'google_t' not in data:
+        graph = facebook.GraphAPI(access_token=data['fb_t'],version='2.7')
+        js = graph.get_object(id='me', fields='first_name,last_name,email,picture')
+        print("js:",js)
+        fname = js['first_name']
+        lname = js['last_name']
+        email = js['email']
+        print("***************************EMAIL************************")
+        print(email)
+        link = js['picture']['data']['url']
+        mem_found = memberExists_by_email(email)
+        print(mem_found)
+        if mem_found:
+            mem = getMember(email)
+            socket.emit("login status", {
+                        'authorized': 1, 'user': mem}, room=request.sid)
+        else:
+            new_mem = registerMember("",fname,lname,email,link)
+            
+            socket.emit("login status", {'authorized': 1,'user':new_mem}, room=request.sid)
+
+
+@socket.on('new message')
+def on_new_message(data):
+    print('***********************************triggered new message********************************')
+    floor_id = data['floor']
+    member_id = data['from']
+    text = data['message']
+    add_message(floor_id, member_id, text)
+    socket.emit("message added", {
+                'floor_messages': getFloorMessages(floor_id)}, room=request.sid)
+
+
+@app.route('/floors')
+@socket.on('create')
+def on_create(data):
+    new_floor = floor(data['floor_name'],data['m_id'],data['isPublic'])
+    socket.emit('floor created', {'floor_id':new_floor.floor_id})
+
+
+
+# def get_dt_ms():
+# 	epoch = datetime.datetime.utcfromtimestamp(0)
+
+#     return (dt - epoch).total_seconds() * 1000.0
+
+if __name__ == '__main__':
+    socket.run(
+        app,
+        port=int(os.getenv('PORT', '80')),
+        host=os.getenv('IP', '0.0.0.0'),
+        debug=True)
+
+ # waht did you do yesterday?
+ # what did you do toda?
+ # what is the plan for today?
+ # roadblocks?
--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,14 +1,15 @@
 #unit_tests.py
 import unittest
 import discoSounds as ds
-import schema as db
+from schema import *
 import random
-
+import serv
 class sc_test(unittest.TestCase):
+	db.app = serv.app
 
-	def test_get_songs(self):
-		response = ds.getSongList('punk')
-		self.assertIsNotNone(response)
+	# def test_get_songs(self):
+	# 	response = ds.getSongList('punk')
+	# 	self.assertIsNotNone(response)
 
 	def test_add_user(self):
 		i = random.randint(1,100000)
@@ -18,17 +19,28 @@ class sc_test(unittest.TestCase):
 		lname = "last_n"+str(i)
 		email = "thisiaanemail@"+str(i)
 		img = 'link' + str(i)
-		new_member = db.registerMember(usnm,fname,lname,email,img)
+		new_member = registerMember(usnm,fname,lname,email,img)
 		self.assertIsNotNone(new_member)
 
 	def test_add_message(self):
-		nm = db.add_message(1,1,"this is a test...")
+		nm = add_message(1,1,"this is a test...nowwww")
 		self.assertEqual(nm,True)
 
 	def test_get_messages(self):
-		messages = db.getFloorMessages(1)
-		print(messages)
-		print(messages[0])
+		messages = getFloorMessages(1)
+		# print(messages)
+		# print(messages[0])
 		self.assertIsNotNone(messages)
+
+	# def test_user_by_email(self):
+	# 	member = db.memberExists_by_email('thisiaanemail@56735')
+	# 	print("memberrrrrrrrrrr")
+	# 	print(member)
+	# 	mem_to_list = db.getMember('thisiaanemail@56735')
+	# 	print(mem_to_list)
+
+	# 	print(db.memberExists_by_email('thisanemaillll@'))
+	# 	self.assertIsNotNone(member)
+
 if __name__ == '__main__':
     unittest.main()
