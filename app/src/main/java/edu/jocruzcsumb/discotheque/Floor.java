3ee4e906d047c4dd4d2930cfa7f85d91b07a31cf
Carsen Yates <fatburgeriii@gmail.com>
2017-04-07 17:57:36 -0700

    Refactored Song.java and added parse method

--- app/src/main/java/edu/jocruzcsumb/discotheque/Floor.java
+++ app/src/main/java/edu/jocruzcsumb/discotheque/Floor.java
@@ -1,12 +1,17 @@
 package edu.jocruzcsumb.discotheque;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+
+import static junit.framework.Assert.fail;
 
 /**
  * Created by Tommy on 3/23/2017.
  */
 
-public class Room {
+public class Room
+{
 
     private String floorName;
     private int numOfUsers;
@@ -14,7 +19,8 @@ public class Room {
     private UserList userList;
     private ArrayList<UserChatMessage> chatList;
 
-    public Room(){
+    public Room()
+    {
         floorName = "blank";
         numOfUsers = 0;
         songList = new SongList();
@@ -23,48 +29,54 @@ public class Room {
     }
 
 
-    public String getFloorName(){
+
+    public static Room parseRoom(JSONObject jsonRoom)
+    {
+        //TODO
+        fail("nyi");
+        return null;
+    }
+
+
+
+    public String getFloorName()
+    {
         return floorName;
     }
 
-    public int getNumOfUsers(){
-        return numOfUsers;
-    }
-
-    public void setFloorName(String floorName){
-        this.floorName = floorName;
-    }
-
-    public void addAllSong(Song song){
-        songList.addSong(song);
-    }
-
-    public void addUserToList(User user){
+    public void addUserToList(User user)
+    {
         userList.addUser(user);
         numOfUsers++;
     }
 
-    public void addChatMessage(UserChatMessage chatMessage){
+    public void addChatMessage(UserChatMessage chatMessage)
+    {
         chatList.add(chatMessage);
     }
 
-    public ArrayList<UserChatMessage> getChatList(){
+    public ArrayList<UserChatMessage> getChatList()
+    {
         return chatList;
     }
 
-    public UserList getUserList(){
+    public UserList getUserList()
+    {
         return userList;
     }
 
-    public SongList getSongList(){
+    public SongList getSongList()
+    {
         return songList;
     }
 
-    public boolean removeUserFromList(User user){
-        if(userList.deleteUser(user)){
+    public boolean removeUserFromList(User user)
+    {
+        if (userList.deleteUser(user))
+        {
             numOfUsers--;
             return true;
-    }
+        }
 
         return false;
     }
--- app/src/main/java/edu/jocruzcsumb/discotheque/Song.java
+++ app/src/main/java/edu/jocruzcsumb/discotheque/Song.java
@@ -1,57 +1,101 @@
 package edu.jocruzcsumb.discotheque;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by Tommy on 3/23/2017.
  */
 
-public class Song implements Comparable<Song> {
+public class Song implements Comparable<Song>
+{
 
-    private String songName;
+    private String title;
     private String artist;
-    private String song_uri;
-    private String photo_link;
+    private String streamUrl;
+    private String artworkUrl;
 
-    public Song(){
-        songName = "";
-        artist = "";
-        song_uri = "";
-        photo_link =  "";
+    public static final String JSON_TITLE_TAG = "title";
+    public static final String JSON_ARTIST_TAG = "creator_user";
+    public static final String JSON_STREAM_URL_TAG = "stream_url";
+    public static final String JSON_ARTWORK_TAG = "artwork";
+
+    public Song()
+    {
+
     }
 
-    public String getPhoto_link(){return photo_link;}
-
-    public String getSongName(){
-        return songName;
+    public Song(String title, String artist, String streamUrl, String artworkUrl)
+    {
+        this();
+        this.title = title;
+        this.artist = artist;
+        this.streamUrl = streamUrl;
+        this.artworkUrl = artworkUrl;
     }
 
-    public String getArtist(){
+    public static Song parse(JSONObject jsonSong) throws JSONException
+    {
+        String title = jsonSong.getString(JSON_TITLE_TAG);
+        String artist = jsonSong.getString(JSON_ARTIST_TAG);
+        String streamUrl = jsonSong.getString(JSON_STREAM_URL_TAG);
+        String artworkUrl = jsonSong.getString(JSON_ARTWORK_TAG);
+        return new Song(title, artist, streamUrl, artworkUrl);
+    }
+
+    public String getArtworkUrl()
+    {
+        return artworkUrl;
+    }
+
+    public String getName()
+    {
+        return title;
+    }
+
+    public String getArtist()
+    {
         return artist;
     }
 
-    public String getSong_uri(){
-        return song_uri;
+    public String getUrl()
+    {
+        return streamUrl;
     }
 
-    public void setPhoto_link(String photo_link){this.photo_link = photo_link;}
-
-    public void setSongName(String songName){
-        this.songName = songName;
+    public void setArtworkUrl(String photo_link)
+    {
+        this.artworkUrl = photo_link;
     }
 
-    public void setArtist(String artist){
+    public void setName(String songName)
+    {
+        this.title = songName;
+    }
+
+    public void setArtist(String artist)
+    {
         this.artist = artist;
     }
 
-    public void setSong_uri(String song_uri){
-        this.song_uri = song_uri;
+    public void setUrl(String song_uri)
+    {
+        this.streamUrl = song_uri;
     }
 
-    public int compareTo(Song compareSong){
-        if(this.songName.compareTo(compareSong.getSongName()) > 0)
+    public int compareTo(Song other)
+    {
+        if (this.title.compareTo(other.getName()) > 0)
+        {
             return 1;
-        else if(this.songName.compareTo(compareSong.getSongName()) < 0)
+        }
+        else if (this.title.compareTo(other.getName()) < 0)
+        {
             return -1;
+        }
         else
+        {
             return 0;
+        }
     }
 }
