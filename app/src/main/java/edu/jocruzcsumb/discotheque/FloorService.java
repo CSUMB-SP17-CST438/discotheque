14370b14f6b3e17d91c30186086b0e0563f7b5bb
Carsen Yates <fatburgeriii@gmail.com>
2017-04-08 14:34:28 -0700

    annotated events (and added some more) in FloorService; Added BroadCastReciever for FloorService

--- app/src/main/java/edu/jocruzcsumb/discotheque/FloorService.java
+++ app/src/main/java/edu/jocruzcsumb/discotheque/FloorService.java
@@ -1,8 +1,10 @@
 package edu.jocruzcsumb.discotheque;
 
 import android.app.IntentService;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
@@ -10,6 +12,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 import io.socket.emitter.Emitter;
@@ -20,24 +23,52 @@ import io.socket.emitter.Emitter;
 public class FloorService extends IntentService
 {
     public static final String TAG = "FloorService";
+
+    // When the Song List is updated for the Floor
     public static final String EVENT_SONG_LIST_UPDATE = "song list";
+
+    // When the Message List is updated for the Floor
     public static final String EVENT_USER_LIST_UPDATE = "user list";//TODO
+
+    // When the Message List is updated for the Floor
     public static final String EVENT_MESSAGE_LIST_UPDATE = "message list";//TODO
 
+    // When the UI requests the most recent Song List
     public static final String EVENT_GET_SONG_LIST = "get song list";
+
+    // When the UI requests the most recent Message List
     public static final String EVENT_GET_MESSAGE_LIST = "get message list";
+
+    // When the UI requests the most recent User List
     public static final String EVENT_GET_USER_LIST = "get user list";
 
+    // When a member leaves the Floor that LocalUser is in
     public static final String EVENT_USER_REMOVE = "remove member";
+
+    // When a member joins the Floor that LocalUser is in
     public static final String EVENT_USER_ADD = "add member";
 
+    // When there is a new message from the server to add to the Floor
     public static final String EVENT_MESSAGE_ADD = "add message";
 
-    public static final String EVENT_JOIN_ROOM = "join floor";
-    public static final String EVENT_ROOM_JOINED = ""; //TODO
+    //When the current LocalUser sends a message
+    public static final String EVENT_MESSAGE_SEND = "send message";
 
-    private static final String ACTION_JOIN_ROOM = "edu.jocruzcsumb.discotheque.action.JOINROOM";
-    private static final String EXTRA_ROOM = "edu.jocruzcsumb.discotheque.extra.ROOM";
+    // When the user requests to join the floor
+    public static final String EVENT_JOIN_FLOOR = "join floor";
+
+    // When the user requests to leave the floor
+    public static final String EVENT_LEAVE_FLOOR = "leave floor";
+
+    // When the server acknowledges that the client has joined the floor
+    // This event also contains the entire floor object according to Ryan
+    public static final String EVENT_FLOOR_JOINED = ""; //TODO
+
+    // The action that is sent to start the FloorService
+    private static final String ACTION_JOIN_FLOOR = "edu.jocruzcsumb.discotheque.action.JOINROOM";
+
+    // Probably will not use this.
+    private static final String EXTRA_FLOOR = "edu.jocruzcsumb.discotheque.extra.ROOM";
 
     private static final Floor floor = null;
 
@@ -54,8 +85,8 @@ public class FloorService extends IntentService
     public static void joinRoom(Context context, String room)
     {
         Intent intent = new Intent(context, FloorService.class);
-        intent.setAction(ACTION_JOIN_ROOM);
-        intent.putExtra(EXTRA_ROOM, room);
+        intent.setAction(ACTION_JOIN_FLOOR);
+        intent.putExtra(EXTRA_FLOOR, room);
         context.startService(intent);
     }
 
@@ -65,9 +96,9 @@ public class FloorService extends IntentService
         if (intent != null)
         {
             final String action = intent.getAction();
-            if (ACTION_JOIN_ROOM.equals(action))
+            if (ACTION_JOIN_FLOOR.equals(action))
             {
-                final String param1 = intent.getStringExtra(EXTRA_ROOM);
+                final String param1 = intent.getStringExtra(EXTRA_FLOOR);
                 handleActionJoinRoom(param1);
             }
         }
@@ -79,14 +110,33 @@ public class FloorService extends IntentService
                              .sendBroadcast(k);
     }
 
+    private void broadcast(String event, Serializable params)
+    {
+        Intent k = new Intent(event);
+        k.putExtra(event, params);
+        broadcast(k);
+    }
+
     /**
      * Handle action Join room in the provided background thread with the provided
      * parameters.
      */
     private void handleActionJoinRoom(String room)
     {
+
+        // This tells the service what LocalBroadcast Events to listen for
+        IntentFilter f = new IntentFilter();
+        f.addAction(FloorService.EVENT_GET_SONG_LIST);
+        f.addAction(FloorService.EVENT_GET_USER_LIST);
+        f.addAction(FloorService.EVENT_GET_MESSAGE_LIST);
+        f.addAction(FloorService.EVENT_LEAVE_FLOOR);
+        f.addAction(FloorService.EVENT_MESSAGE_SEND);
+
+        // Set the activity to listen for app broadcasts with the above filter
+        LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(r, f);
+
         //TODO get the current floor from server
-        Sockets.SocketWaiter waiter = new Sockets.SocketWaiter(EVENT_JOIN_ROOM, EVENT_ROOM_JOINED);
+        Sockets.SocketWaiter waiter = new Sockets.SocketWaiter(EVENT_JOIN_FLOOR, EVENT_FLOOR_JOINED);
 
         // List Events
         Sockets.getSocket()
@@ -101,9 +151,7 @@ public class FloorService extends IntentService
                        floor.setSongs(parseSongList((JSONArray) args[0]));
 
                        // Broadcast the event (so that RoomActivity can update)
-                       Intent k = new Intent(EVENT_SONG_LIST_UPDATE);
-                       k.putExtra(EVENT_SONG_LIST_UPDATE, floor.getSongs());
-                       broadcast(k);
+                       broadcast(EVENT_SONG_LIST_UPDATE, floor.getSongs());
                    }
                });
         Sockets.getSocket()
@@ -118,9 +166,7 @@ public class FloorService extends IntentService
                        floor.setUsers(parseUserList((JSONArray) args[0]));
 
                        // Broadcast the event (so that RoomActivity can update)
-                       Intent k = new Intent(EVENT_USER_LIST_UPDATE);
-                       k.putExtra(EVENT_USER_LIST_UPDATE, floor.getUsers());
-                       broadcast(k);
+                       broadcast(EVENT_USER_LIST_UPDATE, floor.getUsers());
                    }
                });
         Sockets.getSocket()
@@ -135,9 +181,7 @@ public class FloorService extends IntentService
                        floor.setMessages(parseMessages((JSONArray) args[0]));
 
                        // Broadcast the event (so that RoomActivity can update)
-                       Intent k = new Intent(EVENT_MESSAGE_LIST_UPDATE);
-                       k.putExtra(EVENT_MESSAGE_LIST_UPDATE, floor.getMessages());
-                       broadcast(k);
+                       broadcast(EVENT_MESSAGE_LIST_UPDATE, floor.getMessages());
                    }
                });
 
@@ -168,9 +212,7 @@ public class FloorService extends IntentService
                             .add(u);
 
                        // Broadcast the event (so that RoomActivity can update)
-                       Intent k = new Intent(EVENT_USER_ADD);
-                       k.putExtra(EVENT_USER_ADD, u);
-                       broadcast(k);
+                       broadcast(EVENT_USER_ADD, u);
                    }
                });
         Sockets.getSocket()
@@ -199,9 +241,7 @@ public class FloorService extends IntentService
                             .remove(u);
 
                        // Broadcast the event (so that RoomActivity can update)
-                       Intent k = new Intent(EVENT_USER_REMOVE);
-                       k.putExtra(EVENT_USER_REMOVE, u);
-                       broadcast(k);
+                       broadcast(EVENT_USER_REMOVE, u);
                    }
                });
         Sockets.getSocket()
@@ -230,13 +270,39 @@ public class FloorService extends IntentService
                             .add(m);
 
                        // Broadcast the event (so that RoomActivity can update)
-                       Intent k = new Intent(EVENT_MESSAGE_ADD);
-                       k.putExtra(EVENT_MESSAGE_ADD, m);
-                       broadcast(k);
+                       broadcast(EVENT_MESSAGE_ADD, m);
                    }
                });
     }
 
+    BroadcastReceiver r = new BroadcastReceiver()
+    {
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            Log.d(TAG, "onRecieve");
+            Log.d(TAG, "intent.getAction() = " + intent.getAction());
+            switch (intent.getAction())
+            {
+                case FloorService.EVENT_GET_SONG_LIST:
+                    Log.d(TAG, EVENT_GET_SONG_LIST);
+
+                    break;
+                case FloorService.EVENT_GET_USER_LIST:
+                    Log.d(TAG, EVENT_GET_USER_LIST);
+
+                    break;
+                case FloorService.EVENT_MESSAGE_SEND:
+                    Log.d(TAG, EVENT_MESSAGE_SEND);
+
+                    break;
+                case FloorService.EVENT_LEAVE_FLOOR:
+                    Log.d(TAG, EVENT_LEAVE_FLOOR);
+
+                    break;
+            }
+        }
+    };
     private ArrayList<Song> parseSongList(JSONArray a)
     {
         if (a != null)
