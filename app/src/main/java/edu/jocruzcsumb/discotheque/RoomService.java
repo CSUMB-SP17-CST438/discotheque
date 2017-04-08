7100f289cc50a45f93f54054b2197cb0154e13da
Carsen Yates <fatburgeriii@gmail.com>
2017-04-07 19:22:23 -0700

    outlining all Floor socket events

--- app/src/main/java/edu/jocruzcsumb/discotheque/RoomService.java
+++ app/src/main/java/edu/jocruzcsumb/discotheque/RoomService.java
@@ -20,31 +20,25 @@ public class RoomService extends IntentService
 {
     public static final String TAG = "RoomService";
     public static final String EVENT_SONG_LIST_UPDATE = "song list";
+    public static final String EVENT_USER_LIST_UPDATE = "user list";//TODO
+    public static final String EVENT_MESSAGE_LIST_UPDATE = "message list";//TODO
+
     public static final String EVENT_GET_SONG_LIST = "get song list";
-    public static final String EVENT_USER_REMOVE = "member left";
-    public static final String EVENT_USER_ADD = "member joined";
-    public static final String EVENT_NEW_MESSAGE = "message added";
+    public static final String EVENT_GET_MESSAGE_LIST = "get message list";
+    public static final String EVENT_GET_USER_LIST = "get user list";
 
-    public static final String MESSAGE_FROM_TAG = "member";
-    public static final String MESSAGE_FLOOR_TAG = "floor";
-    public static final String MESSAGE_TEXT_TAG = "text";
-    public static final String MESSAGE_PUB_TIME_TAG = "pubTime";
-    public static final String MESSAGE_ID_TAG = "mess_id";
+    public static final String EVENT_USER_REMOVE = "remove member";
+    public static final String EVENT_USER_ADD = "add member";
 
-    public static final String USER_USERNAME_TAG = "username";
-    public static final String USER_CREATED_FLOORS_TAG = "created_floors";
-    public static final String USER_FNAME_TAG = "member_FName";
-    public static final String USER_LNAME_TAG = "member_LName";
-    public static final String USER_IMG_URL_TAG = "member_img_url";
+    public static final String EVENT_NEW_MESSAGE = "add message";
 
-    public static final String SONG_NAME_TAG = "title";
-    public static final String SONG_ARTIST_TAG = "creator_user";
-    public static final String SONG_STREAM_URL_TAG = "stream_url";
-    public static final String SONG_ARTWORK_TAG = "artwork";
+    public static final String EVENT_JOIN_ROOM = "join floor";
+    public static final String EVENT_ROOM_JOINED = ""; //TODO
 
     private static final String ACTION_JOIN_ROOM = "edu.jocruzcsumb.discotheque.action.JOINROOM";
-
     private static final String EXTRA_ROOM = "edu.jocruzcsumb.discotheque.extra.ROOM";
+
+    private static final Floor floor = null;
 
     public RoomService()
     {
@@ -78,15 +72,17 @@ public class RoomService extends IntentService
         }
     }
 
-    private static ArrayList<Song> songs = null;
-    private static ArrayList<Message> messages = null;
-
     /**
      * Handle action Join room in the provided background thread with the provided
      * parameters.
      */
     private void handleActionJoinRoom(String room)
     {
+        //TODO get the current floor from server
+
+        Sockets.SocketWaiter waiter = new Sockets.SocketWaiter(EVENT_JOIN_ROOM, EVENT_ROOM_JOINED);
+
+
         Sockets.getSocket()
                .on(EVENT_SONG_LIST_UPDATE, new Emitter.Listener()
                {
@@ -94,7 +90,29 @@ public class RoomService extends IntentService
                    public void call(Object... args)
                    {
                        Log.d(TAG, EVENT_SONG_LIST_UPDATE);
-                       songs = parseSongList((JSONArray) args[0]);
+                       floor.setSongs(parseSongList((JSONArray) args[0]));
+                   }
+               });
+
+        Sockets.getSocket()
+               .on(EVENT_USER_LIST_UPDATE, new Emitter.Listener()
+               {
+                   @Override
+                   public void call(Object... args)
+                   {
+                       Log.d(TAG, EVENT_USER_LIST_UPDATE);
+                       floor.setUsers(parseUserList((JSONArray) args[0]));
+                   }
+               });
+
+        Sockets.getSocket()
+               .on(EVENT_MESSAGE_LIST_UPDATE, new Emitter.Listener()
+               {
+                   @Override
+                   public void call(Object... args)
+                   {
+                       Log.d(TAG, EVENT_MESSAGE_LIST_UPDATE);
+                       floor.setMessages(parseMessages((JSONArray) args[0]));
                    }
                });
         Sockets.getSocket()
@@ -128,7 +146,7 @@ public class RoomService extends IntentService
                        Log.d(TAG, EVENT_NEW_MESSAGE);
                        try
                        {
-                           messages.add(Message.parse((JSONObject) args[0]));
+                           floor.getMessages().add(Message.parse((JSONObject) args[0]));
                        }
                        catch (JSONException e)
                        {
@@ -152,6 +170,52 @@ public class RoomService extends IntentService
                     songList.add(Song.parse(a.getJSONObject(i)));
                 }
                 return songList;
+            }
+            catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    private ArrayList<Message> parseMessages(JSONArray a)
+    {
+        if (a != null)
+        {
+            try
+            {
+                Log.d(TAG, a.toString());
+                int arrayLength = a.length();
+                ArrayList<Message> messageList = new ArrayList<Message>();
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    messageList.add(Message.parse(a.getJSONObject(i)));
+                }
+                return messageList;
+            }
+            catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    private ArrayList<User> parseUserList(JSONArray a)
+    {
+        if (a != null)
+        {
+            try
+            {
+                Log.d(TAG, a.toString());
+                int arrayLength = a.length();
+                ArrayList<User> userList = new ArrayList<User>();
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    userList.add(User.parse(a.getJSONObject(i)));
+                }
+                return userList;
             }
             catch (JSONException e)
             {
