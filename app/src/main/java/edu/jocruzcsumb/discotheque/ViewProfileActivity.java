d1305dba9a2cc37eb2ef6b8b7232ad7017828146
tommy <tha@csumb.edu>
2017-04-18 15:13:22 -0700

    user profile is now clickable.

--- app/src/main/java/edu/jocruzcsumb/discotheque/ViewProfileActivity.java
+++ app/src/main/java/edu/jocruzcsumb/discotheque/ViewProfileActivity.java
@@ -7,7 +7,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -17,11 +16,8 @@ import org.json.JSONObject;
 public class ViewProfileActivity extends AppCompatActivity implements View.OnClickListener
 {
 
-	private EditText editFirstName;
-	private EditText editLastName;
-	private EditText editEmail;
-	private EditText editDescription;
-	private EditText editGenres;
+
+	private TextView editBio;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -30,30 +26,28 @@ public class ViewProfileActivity extends AppCompatActivity implements View.OnCli
 		setContentView(R.layout.activity_view_profile);
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
-		editFirstName = (EditText) findViewById(R.id.firstName);
-		editLastName = (EditText) findViewById(R.id.lastName);
-		editEmail = (EditText) findViewById(R.id.email);
-		editDescription = (EditText) findViewById(R.id.description);
-		editGenres = (EditText) findViewById(R.id.genres);
-		TextView viewFriends = (TextView) findViewById(R.id.friends);
+		editBio = (TextView) findViewById(R.id.bio);
 
 		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 		fab.setOnClickListener(this);
-		viewFriends.setOnClickListener(this);
-
 		//pass object from previous activity
 		Intent in = getIntent();
 		User user = (User) in.getParcelableExtra("user");
-
-		editFirstName.setText(user.getFirstName());
-		editLastName.setText(user.getLastName());
-		editDescription.setText(user.getBio());
-
 		//TODO: TOMMY, YOU ONLY GET EMAIL FOR LOCALUSER
-		//editEmail.setText(user.getEmail());
-		//editGenres.setText(user.get);
 
-
+		if(user != null) {
+//			if(!LocalUser.getCurrentUser().equals(user)){
+//				editFirstName.setEnabled(false);
+//				editLastName.setEnabled(false);
+//				editDescription.setEnabled(false);
+//				editGenres.setEnabled(false);
+//				fab.setVisibility(View.GONE);
+//			}
+			getSupportActionBar().setTitle(user.getFirstName() + " " + user.getLastName());
+			if(user.getBio() != null){
+				editBio.setText(user.getBio());
+			}
+		}
 	}
 
 
@@ -63,34 +57,34 @@ public class ViewProfileActivity extends AppCompatActivity implements View.OnCli
 		switch(view.getId())
 		{
 			case R.id.fab:
-				String tempfirstName, tempLastName, tempDescription, tempEmail, tempGenres;
-				tempfirstName = editFirstName.getText().toString();
-				tempLastName = editLastName.getText().toString();
-				tempDescription = editDescription.getText().toString();
-				tempEmail = editEmail.getText().toString();
-				tempGenres = editGenres.getText().toString();
-				JSONObject jsonObject = new JSONObject();
+//				String tempfirstName, tempLastName, tempDescription, tempEmail, tempGenres;
+//				tempfirstName = editFirstName.getText().toString();
+//				tempLastName = editLastName.getText().toString();
+//				tempDescription = editDescription.getText().toString();
+//				tempEmail = editEmail.getText().toString();
+//				tempGenres = editGenres.getText().toString();
+//				JSONObject jsonObject = new JSONObject();
 
-				try
-				{
-					jsonObject.put("firstName", tempfirstName);
-					jsonObject.put("lastName", tempLastName);
-					jsonObject.put("description", tempDescription);
-					jsonObject.put("tempEmail", tempEmail);
-					jsonObject.put("genre", tempGenres);
-				}
-				catch(JSONException e)
-				{
-					e.printStackTrace();
-				}
+//				try
+//				{
+//					jsonObject.put("firstName", tempfirstName);
+//					jsonObject.put("lastName", tempLastName);
+//					jsonObject.put("description", tempDescription);
+//					jsonObject.put("tempEmail", tempEmail);
+//					jsonObject.put("genre", tempGenres);
+//				}
+//				catch(JSONException e)
+//				{
+//					e.printStackTrace();
+//				}
 
-				Sockets.getSocket().emit("update user", jsonObject);
-
-				Snackbar.make(view, "Profile info saved", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
+//				Sockets.getSocket().emit("update user", jsonObject);
+//
+//				Snackbar.make(view, "Profile info saved", Snackbar.LENGTH_LONG)
+//						.setAction("Action", null).show();
 				break;
 
-			case R.id.friends:
+			//case R.id.friends:
 				//Intent guestLogin = new Intent(ViewProfileActivity.this, ViewFriendActivity.class);
 				//startActivity(guestLogin);
 		}
