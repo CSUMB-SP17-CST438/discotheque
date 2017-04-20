2da2682be43689fc7b7a069baf43400ded7267d5
Carsen Yates <fatburgeriii@gmail.com>
2017-04-20 15:36:36 -0700

    FloorListener.java

--- app/src/main/java/edu/jocruzcsumb/discotheque/FloorFragment.java
+++ app/src/main/java/edu/jocruzcsumb/discotheque/FloorFragment.java
@@ -30,90 +30,56 @@ public abstract class FloorFragment extends Fragment
 {
     private static final String TAG = "FloorFragment";
     protected static Floor floor = null;
-    private final BroadcastReceiver r = new BroadcastReceiver()
-    {
-        @Override
-        public void onReceive(Context context, Intent intent)
-        {
-            Log.d(TAG, "onRecieve: " + intent.getAction());
-
-            if (intent.getAction()
-                      .equals(EVENT_FLOOR_JOINED))
-            {
-                floor = intent.getParcelableExtra(EVENT_FLOOR_JOINED);
-                onFloorJoined(floor);
-            }
-            else if (floor != null)
-            {
-                switch (intent.getAction())
-                {
-                    case EVENT_SONG_STARTED:
-                        Song s = intent.getParcelableExtra(EVENT_SONG_STARTED);
-                        onSongStarted(s);
-                        break;
-                    case EVENT_SONG_STOPPED:
-                        Song x = intent.getParcelableExtra(EVENT_SONG_STOPPED);
-                        onSongStopped(x);
-                        break;
-                    case EVENT_SONG_LIST_UPDATE:
-                        ArrayList<Song> songs = intent.getParcelableArrayListExtra(EVENT_SONG_LIST_UPDATE);
-                        floor.setSongs(songs);
-                        onSongListUpdate(songs);
-                        break;
-                    case EVENT_USER_LIST_UPDATE:
-                        ArrayList<User> users = intent.getParcelableArrayListExtra(EVENT_USER_LIST_UPDATE);
-                        floor.setUsers(users);
-                        onUserListUpdate(users);
-                        break;
-                    case EVENT_MESSAGE_LIST_UPDATE:
-                        ArrayList<Message> messages = intent.getParcelableArrayListExtra(EVENT_MESSAGE_LIST_UPDATE);
-                        floor.setMessages(messages);
-                        onMessageListUpdate(messages);
-                        break;
-                    case EVENT_MESSAGE_ADD:
-                        Message m = intent.getParcelableExtra(EVENT_MESSAGE_ADD);
-                        floor.getMessages()
-                             .add(m);
-                        onMessageAdded(m);
-                        break;
-                    case EVENT_USER_ADD:
-                        User u = intent.getParcelableExtra(EVENT_USER_ADD);
-                        floor.getUsers()
-                             .add(u);
-                        onUserAdded(u);
-                        break;
-                    case EVENT_USER_REMOVE:
-                        User r = intent.getParcelableExtra(EVENT_USER_REMOVE);
-                        floor.getUsers()
-                             .remove(r);
-                        onUserRemoved(r);
-                        break;
-                }
-            }
-            else
-            {
-                broadcast(EVENT_GET_FLOOR);
-            }
-        }
-    };
 
     public FloorFragment()
     {
-        // This tells the Fragment what LocalBroadcast Events to listen for
-        IntentFilter f = new IntentFilter();
-        f.addAction(EVENT_FLOOR_JOINED);
-        f.addAction(EVENT_SONG_LIST_UPDATE);
-        f.addAction(EVENT_USER_LIST_UPDATE);
-        f.addAction(EVENT_MESSAGE_LIST_UPDATE);
-        f.addAction(EVENT_USER_ADD);
-        f.addAction(EVENT_USER_REMOVE);
-        f.addAction(EVENT_MESSAGE_ADD);
-        f.addAction(EVENT_SONG_STARTED);
-        f.addAction(EVENT_SONG_STOPPED);
+        FloorListener listener = new FloorListener(getContext())
+        {
+            public void onSongStarted(Song s)
+            {
+                FloorFragment.this.onSongStarted(s);
+            }
 
-        // Set the Fragment to listen for app broadcasts with the above filter
-        LocalBroadcastManager.getInstance(this.getContext())
-                             .registerReceiver(r, f);
+            public void onSongStopped(Song s)
+            {
+                FloorFragment.this.onSongStopped(s);
+            }
+
+            public void onFloorJoined(Floor floor)
+            {
+                FloorFragment.this.onFloorJoined(floor);
+            }
+
+            public void onSongListUpdate(ArrayList<Song> songs)
+            {
+                FloorFragment.this.onSongListUpdate(songs);
+            }
+
+            public void onUserListUpdate(ArrayList<User> users)
+            {
+                FloorFragment.this.onUserListUpdate(users);
+            }
+
+            public void onUserAdded(User u)
+            {
+                FloorFragment.this.onUserAdded(u);
+            }
+
+            public void onUserRemoved(User u)
+            {
+                FloorFragment.this.onUserRemoved(u);
+            }
+
+            public void onMessageListUpdate(ArrayList<Message> messages)
+            {
+                FloorFragment.this.onMessageListUpdate(messages);
+            }
+
+            public void onMessageAdded(Message m)
+            {
+                FloorFragment.this.onMessageAdded(m);
+            }
+        };
     }
     // EVENTS are broadcasted here
     protected void broadcast(String event)
