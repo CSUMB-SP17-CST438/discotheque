6168951f5bfab5d8e1304eb7522162ec836b32b0
Carsen Yates <fatburgeriii@gmail.com>
2017-04-20 23:43:17 -0700

    I AM TOO TIRED TO WORK MORE, the seamlessplayer should be fixed, need to test tomorrow when am not tired

--- app/src/main/java/edu/jocruzcsumb/discotheque/SeamlessMediaPlayer.java
+++ app/src/main/java/edu/jocruzcsumb/discotheque/SeamlessMediaPlayer.java
@@ -85,58 +85,167 @@ public class SeamlessMediaPlayer extends BroadcastReceiver implements MediaPlaye
             Log.d(TAG, "couldnt checkSongs because lock");
             return;
         }
-        if (songs.size() > 0 && s[current] != songs.get(0))
+        lock = true;
+        Song cur = songs.get(0);
+        boolean start = s[current] == null;
+        long localtime = System.currentTimeMillis() / 1000;
+        boolean needSeek = localtime > cur.getStartTime();
+
+        // Check to see that there are songs in the list
+        if (songs.size() <= 0)
         {
-            lock = true;
-            s[current] = songs.get(0);
-            // The currently playing song has been invalidated, stop and restart player[current]
-            if (m[current] != null && m[current].isPlaying())
-            {
-                m[current].stop();
-            }
-            reset(current);
+            Log.e(TAG, "Song list was empty");
+            lock = false;
+            return;
+        }
 
-            long localtime = System.currentTimeMillis() / 1000;
-            long songtime = s[current].getStartTime();
+        // Check to see that the start time is valid
+        if (cur.getStartTime() == 0)
+        {
+            Log.e(TAG, "Start time was invalid");
+            lock = false;
+            return;
+        }
 
-            Log.d(TAG, "CURRENT TIME: " + String.valueOf(localtime));
-            Log.d(TAG, "SONG START TIME: " + String.valueOf(songtime));
-            if (localtime >= songtime)
+        if (!start && cur == s[next])
+        {// The service has told us that the next song is about to play.
+            if(s[current].getStartTime() < localtime && cur.getStartTime() > localtime)
             {
-                m[current].start();
-                m[current].seekTo(1000 * (int) (localtime - songtime));
+                if (!m[current].isPlaying())
+                {
+                    //this is a weird case, the next song is scheduled
+                    // ...but the current song is not playing
+                    prepareNext();
+                    swap();
+                    localtime = System.currentTimeMillis() / 1000;
+                    try
+                    {
+                        Thread.sleep(s[current].getStartTime() - localtime);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                        Log.e(TAG, "Thread.sleep InterruptedException");
+                        return;
+                    }
+                    if(!startCurrent())
+                    {
+                        lock = false;
+                        return;
+                    }
+                }
+                // else: we do not interrupt the songs as they are scheduled correctly.
+                lock = false;
+                return;
             }
-            else //(localtime < songtime)
+            else
             {
-                try
-                {
-                    Thread.sleep(1000 * (songtime - localtime));
-                }
-                catch (InterruptedException e)
-                {
-                    e.printStackTrace();
-                    return;
-                }
-                m[current].start();
+                Log.e(TAG, "WARNING COULD NOT SYNC");
+                lock = false;
+                return;
             }
+        }
+        else
+        {
+            if(start)
+            {
+                // There is no song currently playing
+                // Here we start playing the very first song
+                s[current] = songs.get(0);
+                prepareCurrent();
+                localtime = System.currentTimeMillis() / 1000;
+
+                Log.d(TAG, "CURRENT TIME: " + String.valueOf(localtime));
+                Log.d(TAG, "SONG START TIME: " + String.valueOf(s[current].getStartTime()));
+
+                needSeek = localtime > cur.getStartTime();
+                if (m[current] != null && m[current].isPlaying())
+                {
+                    m[current].stop();
+                }
+                if (needSeek)
+                {
+                    localtime = System.currentTimeMillis() / 1000;
+                    m[current].seekTo(1000 * (int) (localtime - s[current].getStartTime()));
+                    if(!startCurrent())
+                    {
+                        lock = false;
+                        return;
+                    }
+                }
+                else // NOT need seek
+                {
+                    //FIRST try to wait for the time the song is supposed to start
+                    try
+                    {
+                        localtime = System.currentTimeMillis() / 1000;
+                        Thread.sleep(s[current].getStartTime() - localtime);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                        Log.e(TAG, "Thread.sleep InterruptedException");
+                        lock = false;
+                        return;
+                    }
+                    //We waited, now start the song
+                    if(!startCurrent())
+                    {
+                        lock = false;
+                        return;
+                    }
+                }
+            }
+            if(songs.size() > 1 && songs.get(1) != s[next])
+            {
+                s[next] = songs.get(1);
+                prepareNext();
+            }
+        }
+        lock = false;
+    }
+
+    private boolean startCurrent()
+    {
+        try
+        {
+            m[current].start();
             m[current].setOnCompletionListener(this);
             m[current].setOnErrorListener(this);
-            Intent k = new Intent(EVENT_SONG_STARTED);
-            k.putExtra(EVENT_SONG_STARTED, s[current]);
-            LocalBroadcastManager.getInstance(context.getApplicationContext())
-                                 .sendBroadcast(k);
-
-            Log.d(TAG, "started playback");
-            lock = false;
         }
-        if (songs.size() > 1 && s[next] != songs.get(1))
+        catch (Exception e)
         {
-            lock = true;
-            s[next] = songs.get(1);
-            // The next song has been invalidated, reset the player[next]
-            reset(next);
-            lock = false;
+            e.printStackTrace();
+            Log.e(TAG, "Could not start current song");
+            return false;
         }
+        return true;
+    }
+    private boolean prepareCurrent()
+    {
+        return prep(current);
+    }
+    private boolean prepareNext()
+    {
+        return prep(next);
+    }
+    private boolean prep(int i)
+    {
+        try
+        {
+            m[i] = new MediaPlayer();
+            m[i].setAudioStreamType(AudioManager.STREAM_MUSIC);
+            m[i].setDataSource(s[i].getUrl());
+            m[i].setLooping(false);
+            m[i].prepare();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            Log.e(TAG, "Could not prepare song");
+            return false;
+        }
+        return true;
     }
 
     @Override
@@ -160,26 +269,19 @@ public class SeamlessMediaPlayer extends BroadcastReceiver implements MediaPlaye
     @Override
     public void onCompletion(MediaPlayer mediaPlayer)
     {
-        Log.d(TAG, "onCompletion");
+        Log.i(TAG, "onCompletion");
 
         // Swap the mediaplayers
         swap();
+        // Start the new song
+        startCurrent();
+        // Prepare the next song
+        prepareNext();
 
-        //Start the next song
-        m[current].start();
-        m[current].setOnCompletionListener(this);
-        m[current].setOnErrorListener(this);
-
-        //Broadcast
-        Intent k = new Intent(EVENT_SONG_STARTED);
-        k.putExtra(EVENT_SONG_STARTED, s[current]);
-        LocalBroadcastManager.getInstance(context.getApplicationContext())
-                             .sendBroadcast(k);
-
+        // dispose of the player that just finished
         mediaPlayer.release();
-        // Remove the song we just played and then re check the song list
         songs.remove(s[next]);
-        checkSongs();
+        //checkSongs();
     }
 
     @Override
