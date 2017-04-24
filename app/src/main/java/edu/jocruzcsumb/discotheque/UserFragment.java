9138914db9a8766ebeda352a1a0654251364cc42
Margaret M Havens <margaretmhavens@Margarets-MacBook-Air.local>
2017-04-23 18:12:24 -0700

    added card covers

--- app/src/main/java/edu/jocruzcsumb/discotheque/UserFragment.java
+++ app/src/main/java/edu/jocruzcsumb/discotheque/UserFragment.java
@@ -59,153 +59,6 @@ public class UserFragment extends FloorFragment
 	@Override
 	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
 	{
-<<<<<<< HEAD
-        View rootView = inflater.inflate(R.layout.fragment_user, container, false);
-        userPhoto = (ImageView) rootView.findViewById(R.id.userPhoto);
-        username = (TextView) rootView.findViewById(R.id.username);
-        LinearLayoutManager llm = new LinearLayoutManager(this.getActivity());
-        //llm.setStackFromEnd(true); //scrolls to the bottom
-        recyclerView = (RecyclerView) rootView.findViewById(R.id.rv3);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(llm);
-		if(findFloor()) updateListUI(floor.getUsers());
-        return rootView;
-    }
-
-    @Override
-    public void onUserListUpdate(ArrayList<User> users)
-    {
-        updateListUI(users);
-    }
-
-    @Override
-    public void onUserAdded(User u)
-    {
-        updateListUI(floor.getUsers());
-    }
-
-    @Override
-    public void onUserRemoved(User u)
-    {
-        updateListUI(floor.getUsers());
-    }
-
-    public void updateListUI(final ArrayList<User> users)
-    {
-        // check that we can access the parent activity
-        final Activity a = getActivity();
-        if (a == null)
-        {
-            Log.e(TAG, "onSongListUpdate: getActivity returned null, cancelling update");
-            return;
-        }
-
-        userAdapter = new UserFragment.UserAdapter(a, users, new RecyclerViewListener()
-        {
-            @Override
-            public void onItemClick(View v, int position)
-            {
-                User user = users.get(position);
-                Log.d(TAG, user.toString());
-                //TODO: pass user to profile activity
-                Intent intent = new Intent(a, ViewProfileActivity.class);
-                intent.putExtra("user", user);
-                startActivity(intent);
-            }
-        });
-        a.runOnUiThread(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                recyclerView.setAdapter(userAdapter);
-            }
-        });
-    }
-
-
-    public class UserAdapter extends RecyclerView.Adapter<UserFragment.UserAdapter.UserViewHolder>
-    {
-        Context mContext;
-        ArrayList<User> users = null;
-        RecyclerViewListener listener;
-
-
-        UserAdapter(Context mContext, ArrayList<User> users, RecyclerViewListener listener)
-        {
-            this.users = users;
-            this.mContext = mContext;
-            this.listener = listener;
-        }
-
-        public int getItemCount()
-        {
-            return users.size();
-        }
-
-        @Override
-        public UserFragment.UserAdapter.UserViewHolder onCreateViewHolder(ViewGroup viewGroup, int i)
-        {
-            View v = LayoutInflater.from(viewGroup.getContext())
-                                   .inflate(R.layout.list_user, viewGroup, false);
-            final UserFragment.UserAdapter.UserViewHolder svh = new UserFragment.UserAdapter.UserViewHolder(v);
-            v.setOnClickListener(new View.OnClickListener()
-            {
-                @Override
-                public void onClick(View v)
-                {
-
-                    listener.onItemClick(v, svh.getAdapterPosition());
-
-                }
-            });
-
-
-            return svh;
-        }
-
-
-        @Override
-        public void onBindViewHolder(UserFragment.UserAdapter.UserViewHolder userViewHolder, int i)
-        {
-            //TODO set profile picture
-            Picasso.with(mContext)
-                   .load(users.get(i)
-                              .getPhoto())
-                    .transform(new CircleTransform())
-                   .into(userViewHolder.userPhoto);
-
-            userViewHolder.username.setText(users.get(i)
-                                                 .getFirstName() + " " + users.get(i)
-                                                                              .getLastName());
-        }
-
-        @Override
-        public void onAttachedToRecyclerView(RecyclerView recyclerView)
-        {
-            super.onAttachedToRecyclerView(recyclerView);
-        }
-
-        public class UserViewHolder extends RecyclerView.ViewHolder
-        {
-            CardView cv;
-            TextView username;
-            ImageView userPhoto;
-
-            UserViewHolder(View itemView)
-            {
-                super(itemView);
-                cv = (CardView) itemView.findViewById(R.id.userCardView);
-                username = (TextView) itemView.findViewById(R.id.username);
-                userPhoto = (ImageView) itemView.findViewById(R.id.userPhoto);
-
-
-            }
-        }
-
-
-    }
-=======
 		View rootView = inflater.inflate(R.layout.fragment_user, container, false);
 		userPhoto = (ImageView) rootView.findViewById(R.id.userPhoto);
 		username = (TextView) rootView.findViewById(R.id.username);
@@ -307,7 +160,7 @@ public class UserFragment extends FloorFragment
 		public UserFragment.UserAdapter.UserViewHolder onCreateViewHolder(ViewGroup viewGroup, int i)
 		{
 			View v = LayoutInflater.from(viewGroup.getContext())
-								   .inflate(R.layout.list_user, viewGroup, false);
+					.inflate(R.layout.list_user, viewGroup, false);
 			final UserFragment.UserAdapter.UserViewHolder svh = new UserFragment.UserAdapter.UserViewHolder(v);
 			v.setOnClickListener(new View.OnClickListener()
 			{
@@ -329,13 +182,13 @@ public class UserFragment extends FloorFragment
 		{
 			//TODO set profile picture
 			Picasso.with(mContext)
-				   .load(users.get(i)
-							  .getPhoto())
-				   .into(userViewHolder.userPhoto);
+					.load(users.get(i)
+							.getPhoto())
+					.into(userViewHolder.userPhoto);
 
 			userViewHolder.username.setText(users.get(i)
-												 .getFirstName() + " " + users.get(i)
-																			  .getLastName());
+					.getFirstName() + " " + users.get(i)
+					.getLastName());
 		}
 
 		@Override
@@ -363,5 +216,4 @@ public class UserFragment extends FloorFragment
 
 
 	}
->>>>>>> 684bdd59e60edb50f07fa6441d83a9039511697c
 }
